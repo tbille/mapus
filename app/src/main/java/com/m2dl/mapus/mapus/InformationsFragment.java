76758708807483a35da5ac74759435be55e6f7e1
valentinchevalier <valche.vc@gmail.com>
2017-03-04 23:44:31 +0100

    Add soundMeter and luminosityMeter fragments Add m2dl logo

--- app/src/main/java/com/m2dl/mapus/mapus/InformationsFragment.java
+++ app/src/main/java/com/m2dl/mapus/mapus/InformationsFragment.java
@@ -1,10 +1,19 @@
 package com.m2dl.mapus.mapus;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Display app informations
@@ -28,16 +37,12 @@ public class InformationsFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle(R.string.title_fragment_informations);
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_informations, container, false);
     }
 }
