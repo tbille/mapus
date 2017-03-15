4e15f46c768c35c3f448173dd3f2678020d1873d
ClementChaumel <clement.chaumel@gmail.com>
2017-03-15 10:53:47 +0100

    Added geoloc to anomalies. Changed icon and team picture

--- app/src/main/java/com/m2dl/mapus/mapus/GeolocalisationFragment.java
+++ app/src/main/java/com/m2dl/mapus/mapus/GeolocalisationFragment.java
@@ -1,10 +1,26 @@
 package com.m2dl.mapus.mapus;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class GeolocalisationFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
@@ -16,6 +32,12 @@ public class GeolocalisationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private MapView mapView;
+    private MapboxMap map;
+    private FloatingActionButton floatingActionButton;
+    private LocationServices locationServices;
+    private static final int PERMISSIONS_LOCATION = 0;
+
     public GeolocalisationFragment() {
         // Required empty public constructor
     }
@@ -23,9 +45,6 @@ public class GeolocalisationFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment GeolocalisationFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -41,17 +60,122 @@ public class GeolocalisationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        MapboxAccountManager.start(this.getActivity(), getString(R.string.access_token));
+        locationServices = LocationServices.getLocationServices(this.getContext());
+    }
+
+    private void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle(R.string.title_fragment_geoloc);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_geolocalisation, container, false);
+        View view = inflater.inflate(R.layout.fragment_geolocalisation, container, false);
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.location_toggle_fab);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
+
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(43.56274, 1.46920))
+                        .title("El toto")
+                        .snippet("Un animal sauvage en liberté !!!"));
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+                }
+            }
+        });
+        return view;
+    }
+
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            Location lastLocation = locationServices.getLastLocation();
+            if (lastLocation != null) {
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+            }
+
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
+                        locationServices.removeLocationListener(this);
+                    }
+                }
+            });
+            floatingActionButton.setImageResource(R.drawable.ic_location_disabled_24dp);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.ic_my_location_24dp);
+        }
+        map.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableLocation(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
